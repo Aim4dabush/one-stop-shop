9c081d98eb6393c93d3359bb07a42ecca7156252
aim4dabush <edarasato@gmail.com>
2023-05-12 09:14:50 -0700

    added onClick event

--- src/components/shared/shared-button/SharedButton.js
+++ src/components/shared/shared-button/SharedButton.js
@@ -1,12 +1,13 @@
 //styles
 import styles from "./SharedButton.module.scss";
 
-const SharedButton = ({ buttonStyle, children }) => {
+const SharedButton = ({ buttonStyle, children, clickHandler }) => {
   return (
     <button
       className={`${buttonStyle === "nav-link" && styles.nav_link} ${
         buttonStyle === "non-nav-link" && styles.non_nav_link
       } ${buttonStyle === "primary" && styles.primary}`}
+      onClick={clickHandler}
     >
       {children}
     </button>
