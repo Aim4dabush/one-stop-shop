e687ea96c279fc970f4999056a5c31e40ecb9016
aim4dabush <edarasato@gmail.com>
2023-05-12 08:59:11 -0700

    added another button style

--- src/components/shared/shared-button/SharedButton.js
+++ src/components/shared/shared-button/SharedButton.js
@@ -3,7 +3,11 @@ import styles from "./SharedButton.module.scss";
 
 const SharedButton = ({ buttonStyle, children }) => {
   return (
-    <button className={`${buttonStyle === "nav-link" && styles.nav_link}`}>
+    <button
+      className={`${buttonStyle === "nav-link" && styles.nav_link} ${
+        buttonStyle === "non-nav-link" && styles.non_nav_link
+      }`}
+    >
       {children}
     </button>
   );
