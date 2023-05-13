f4c1f77c73f22eb32722df1f6adbad452545cd32
aim4dabush <edarasato@gmail.com>
2023-05-13 10:19:57 -0700

    edited import

---
+++ src/firebase/services/auth-service.js
@@ -0,0 +1,91 @@
+import {
+  setLoggedUser,
+  setLoggedUserReset,
+} from "../../redux/slices/authSlice";
+
+//firebase
+import { app } from "../firebaseConfig";
+
+//authentication
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+
+//luxon
+import { DateTime } from "luxon";
+
+const auth = getAuth(app);
+
+export const signup = (data) => {
+  return async (dispatch) => {
+    try {
+      const result = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+
+      if (!result.user.uid) {
+        throw new Error("Problem with creating a new user");
+      }
+
+      const user = result.user;
+      updateProfile(user, {
+        displayName: data.name,
+        phoneNumber: data.phone,
+      });
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+};
+
+export const login = (loginInfo) => {
+  return async (dispatch) => {
+    try {
+      const result = await signInWithEmailAndPassword(
+        auth,
+        loginInfo.email,
+        loginInfo.password
+      );
+
+      if (!result.user.uid) {
+        throw new Error("Problem getting user credentials");
+      }
+
+      const user = await result.user.getIdTokenResult();
+
+      if (!user.token) {
+        throw new Error("Problem with getting user token");
+      }
+
+      const expTime = DateTime.fromSeconds(user.claims.exp)
+        .plus({ hours: 2 })
+        .toFormat("X");
+      const currentTime = DateTime.now().toFormat("X");
+      let loginCredentials = {
+        id: user.claims.user_id,
+        expires: expTime - currentTime,
+        token: user.token,
+      };
+
+      localStorage.setItem("user", JSON.stringify(loginCredentials));
+      dispatch(setLoggedUser(loginCredentials));
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+};
+
+export const logout = () => {
+  return (dispatch) => {
+    signOut(auth);
+    dispatch(setLoggedUserReset());
+    localStorage.clear();
+    console.log("logged out");
+  };
+};
