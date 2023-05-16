2b626413653d27f6f4b2ee654e6d6b98343d420c
aim4dabush <edarasato@gmail.com>
2023-05-15 17:54:27 -0700

    consolidated user get calls into one component

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,22 +5,19 @@ import { useNavigate } from "react-router-dom";
 import Login from "../components/login/Login";
 
 //redux
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 //services
-import { getProfile } from "../firebase/services/profile-service";
 
 const LoginPage = () => {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
   const { loggedUser } = useSelector((state) => state.auth);
 
   useEffect(() => {
     if (loggedUser.token) {
-      dispatch(getProfile());
       navigate("/", { replace: true });
     }
-  }, [dispatch, navigate, loggedUser]);
+  }, [navigate, loggedUser]);
   return (
     <section>
       <Login />
